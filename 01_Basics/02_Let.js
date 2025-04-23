758a11f458d078f07232299b8015a8ce8cbaafdd
Rohitpatil90 <154263197+Rohitpatil90@users.noreply.github.com>
2025-04-23 11:02:19 +0000

    Learn about let

---
+++ 01_Basics/02_Let.js
@@ -0,0 +1,18 @@
+/* Let - 
+   
+  Que 1) What is the let in js?
+  Ans - let is the keyword in js used to declare the bloced- scope variables unlike var, 
+        let does not get hoisted to the top of its block scope and also its not attached to the 
+        global window object in brrowser. 
+    
+  Que 2) How let different from var ?
+  Ans - feature                    let                        var 
+        scope                 blocked- scope          function-scoped 
+        re-declaration           no                         yes 
+        hoisting                 yes but undefined           yes 
+        attached to widow         no                         yes 
+
+   Que 3) Can let be updated ? 
+   Ans - yes , */
+
+   
